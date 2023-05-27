1fb69618d055c87f98079c910e487cb5fd3bce1e
Juan Calero <jcalero@broadvoice.com>
2023-05-27 12:36:13 +0100

    added storybook

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  globals: {
-    "ts-jest": {
-      diagnostics: false,
-      tsconfig: {
-        target: "ES2017",
-      },
-      isolatedModules: true,
-    },
-  },
-  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
-  moduleDirectories: ["node_modules"],
-  testPathIgnorePatterns: ["/node_modules/"],
-  testEnvironment: "jsdom",
+	globals: {
+		'ts-jest': {
+			diagnostics: false,
+			tsconfig: {
+				target: 'ES2017',
+			},
+			isolatedModules: true,
+		},
+	},
+	setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+	moduleDirectories: ['node_modules'],
+	testPathIgnorePatterns: ['/node_modules/'],
+	testEnvironment: 'jsdom',
 };
